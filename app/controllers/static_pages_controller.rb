7b51daea4a4f1444e0663f0a20447bc478492de2
Metaphysiker <s.raess@me.com>
2021-10-12 19:51:32 +0200

    added seo gem

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
   def welcome
+    @page_title       = I18n.t("welcome")
+    @page_description = I18n.t("welcome_description")
+    @page_keywords    = I18n.t("welcome_keywords")
   end
 end
