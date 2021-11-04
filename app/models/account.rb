b53bf6a202b21997c9a5233e074f603799b8d779
Metaphysiker <s.raess@me.com>
2021-11-04 20:30:59 +0100

    trying to figure out graph

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,14 @@
 class Account < ApplicationRecord
   belongs_to :user
   has_many :work_times
+
+  def work_time_data
+    data_array = []
+    self.work_times.each do |work_time|
+      data_array.push({date: work_time.created_at.to_date, minutes: work_time.minutes})
+    end
+
+    data_array.to_json
+  end
+
 end
