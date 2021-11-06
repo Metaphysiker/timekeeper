2f5ff2e558e4ec76cefac0492078b0e26776130a
Metaphysiker <s.raess@me.com>
2021-11-06 10:25:47 +0100

    fixed data

--- app/models/account.rb
+++ app/models/account.rb
@@ -7,14 +7,18 @@ class Account < ApplicationRecord
     return [{date: Date.today.to_s, minutes: 0}] if self.work_times.empty?
 
     interval = 1.day if interval.nil?
-    start_date = self.work_times.order(:datetime).limit(1).first.datetime if start_date.nil?
-    end_date = self.work_times.order(:datetime).reverse_order.limit(1).first.datetime if end_date.nil?
+    start_date = self.work_times.order(:datetime).limit(1).first.datetime.to_date if start_date.nil?
+    end_date = self.work_times.order(:datetime).reverse_order.limit(1).first.datetime.to_date if end_date.nil?
 
     data_array = []
 
-    while start_date.before?(end_date)
+    while (start_date === end_date) || (start_date.before?(end_date))
 
-    sum_of_minutes = self.work_times.where(datetime: start_date..start_date + interval).sum(:minutes)
+    puts("loop_start")
+    puts(start_date)
+    puts(end_date)
+
+    sum_of_minutes = self.work_times.where(datetime: start_date..start_date + interval - 1.day).sum(:minutes)
     data_array.push({date: start_date.to_date.to_s, minutes: sum_of_minutes})
 
     start_date = start_date + interval
