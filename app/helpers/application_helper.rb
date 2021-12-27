f507a6e6899d5b2668d69e306c17317a9c529fea
Metaphysiker <s.raess@me.com>
2021-12-27 19:06:24 +0100

    changed welcome

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,16 @@ module ApplicationHelper
     end
     nil
   end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @user ||= User.new
+  end
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
 end
