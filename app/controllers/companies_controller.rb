cd55ae261b0d95e5a79ce3ff9e99aaadc40f9d4c
Metaphysiker <s.raess@me.com>
2021-12-29 06:57:39 +0100

    updating company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -64,6 +64,6 @@ class CompaniesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def company_params
-      params.require(:company).permit(:name, :email)
+      params.require(:company).permit(:name)
     end
 end
