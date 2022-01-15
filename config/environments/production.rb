9e5a90b1798bf564ec0b08205e61008d0fd55846
Metaphysiker <s.raess@me.com>
2022-01-15 16:16:14 +0100

    m

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -123,6 +123,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.assets.js_compressor = :uglifier # make sure the 'uglifier' gem is included before adding this line
-
+  config.assets.js_compressor = Uglifier.new(harmony: true)
 end
